3fc06d35b25a034dc7425c6da8e3e93c54f19b35
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-11 08:49:46 -0600

    added q1: user story

---
+++ snap-7-11/q1.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>User Story</title>
+	</head>
+	<body>
+		<div>
+			<h2>User Story</h2>
+			<p>As a power user I can find image urls</p>
+		</div>
+	</body>
+</html>
